08e8f3164eda85e028d9ca64c808245a5836ebf0
TunNandaAung <tunnandaaung@outlook.com>
2019-11-21 21:34:42 +0630

    Change PHP to store image to DB

--- php_backend/file-upload.php
+++ php_backend/file-upload.php
@@ -5,46 +5,38 @@ header("Access-Control-Allow-Methods: PUT, GET, POST");
 $response = array();
 $upload_dir = 'uploads/';
 $server_url = 'http://127.0.0.1:8000';
-if($_FILES['avatar'])
-{
-    $avatar_name = $_FILES["avatar"]["name"];
-    $avatar_tmp_name = $_FILES["avatar"]["tmp_name"];
-    $error = $_FILES["avatar"]["error"];
-    if($error > 0){
+if ( isset($_FILES['image']['tmp_name']) ) {
+    // open mysqli db connection
+    $mysqli = new mysqli('127.0.0.1:3306','root','','images');
+
+    // get image data
+    $binary = file_get_contents($_FILES['image']['tmp_name']);
+
+    // get mime type
+    $finfo = new finfo(FILEINFO_MIME);
+    $type = $finfo->file($_FILES['image']['tmp_name']);
+    $mime = substr($type, 0, strpos($type, ';'));
+
+    $query = "INSERT INTO `images` 
+                    (`data`,`mime`,`name`) 
+    VALUES('".$mysqli->real_escape_string($binary)."',
+            '".$mysqli->real_escape_string($mime)."',
+            '".$mysqli->real_escape_string($_FILES['image']['name'])."')";
+
+    if($mysqli->query($query)){
+        $response = array(
+            "status" => "success",
+            "error" => false,
+            "message" => "File uploaded successfully",
+        );
+    }
+    else{
         $response = array(
             "status" => "error",
             "error" => true,
             "message" => "Error uploading the file!"
         );
-    }else
-    {
-        $random_name = rand(1000,1000000)."-".$avatar_name;
-        $upload_name = $upload_dir.strtolower($random_name);
-        $upload_name = preg_replace('/\s+/', '-', $upload_name);
-
-        if(move_uploaded_file($avatar_tmp_name , $upload_name)) {
-            $response = array(
-                "status" => "success",
-                "error" => false,
-                "message" => "File uploaded successfully",
-                "url" => $server_url."/".$upload_name
-            );
-        }else
-        {
-            $response = array(
-                "status" => "error",
-                "error" => true,
-                "message" => "Error uploading the file!"
-            );
-        }
     }
-
-}else{
-    $response = array(
-        "status" => "error",
-        "error" => true,
-        "message" => "No file was sent!"
-    );
 }
 echo json_encode($response);
 ?>
